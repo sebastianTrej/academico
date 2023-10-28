ee0263fcb11ea61a20f54c1a98a92ddce892096a
sebastianTrej <andrestrejo7777777@gmail.com>
2023-10-27 19:59:48 -0500

    clase 2-3 corte

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=500)
+    dni = models.CharField(max_length=10, default='')
